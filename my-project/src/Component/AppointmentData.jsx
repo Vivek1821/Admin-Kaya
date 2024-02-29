765baca8527bff09480e80a212c9cbcd3829043e
Vivek <mahajanvivek128@gmail.com>
2024-03-01 02:25:04 +0530

    appointment

---
+++ my-project/src/Component/AppointmentData.jsx
@@ -0,0 +1,91 @@
+import React, { useState, useEffect } from "react";
+
+const AppointmentData = () => {
+  const [appointments, setAppointments] = useState([]);
+
+  useEffect(() => {
+    // Fetch appointment data from the API
+    fetch("https://65e09bb8d3db23f76249b70d.mockapi.io/calendar-data")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setAppointments(data); // Update state with fetched data
+      })
+      .catch((error) => {
+        console.error("Error fetching appointment data from API:", error);
+        // Handle errors here
+      });
+  }, []); // Empty dependency array ensures useEffect runs only once
+
+  return (
+    <div className="text-black w-full overflow-hidden border-[1px] border-black rounded-xl ml-4">
+      <div className="flex flex-col">
+        <div className="-m-1.5 overflow-x-auto">
+          <div className="p-1.5 min-w-full inline-block align-middle">
+            <div className="overflow-hidden">
+              <table className="min-w-full divide-y divide-gray-200">
+                <thead>
+                  <tr>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
+                    >
+                      Name
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
+                    >
+                      Email
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
+                    >
+                      Contact Number
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
+                    >
+                      Selected Time Slot
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {appointments.map((appointment, index) => (
+                    <tr
+                      key={index}
+                      className={`${
+                        index % 2 === 0 ? "even:bg-gray-100 " : ""
+                      }odd:bg-white even:bg-gray-100`}
+                    >
+                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 w-1/4">
+                        {appointment.name}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 w-1/4">
+                        {appointment.email}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 w-1/4">
+                        {appointment.contactNumber}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 w-1/4">
+                        {appointment.selectedTimeSlot}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AppointmentData;
--- my-project/src/Component/Chart.jsx
+++ my-project/src/Component/Chart.jsx
@@ -1,32 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { VictoryPie } from "victory";
 import CardContainer from "./CardContainer";
+import AppointmentData from "./AppointmentData";
 
 const Chart = () => {
-  const sampleData = [
-    { x: "Cats", y: 35 },
-    { x: "Dogs", y: 40 },
-    { x: "Birds", y: 55 },
-  ];
+  const [serviceData, setServiceData] = useState([]);
+  const [colorScale, setColorScale] = useState([
+    "tomato",
+    "orange",
+    "gold",
+    "cyan",
+    "navy",
+  ]);
 
-  const colorScale = ["tomato", "orange", "gold", "cyan", "navy"];
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://65dcb4ebe7edadead7ecbc41.mockapi.io/api/immigration/services"
+        );
+        const data = await response.json();
+        // Extracting titles and values from the API response
+        const extractedData = data.map((item) => ({
+          x: item.title,
+          y: 30,
+        }));
+        setServiceData(extractedData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
-    <div className="flex justify-start w-full">
+    <div className="flex justify-start w-96 gap-2">
       <div className="bg-white rounded-lg shadow-2xl p-6">
         <div className="text-xl font-bold mb-4">User Data</div>
         <div className="flex flex-col">
           <div className="w-72">
             <VictoryPie
               colorScale={colorScale}
-              data={sampleData}
-              style={{
-                width: "",
-              }}
+              data={serviceData}
+              style={{ width: "300px", height: "300px" }}
             />
           </div>
           <div className="ml-8 items-start">
-            {sampleData.map((item, index) => (
+            {serviceData.map((item, index) => (
               <div key={index} className="flex items-center mb-2">
                 <div
                   className="w-4 h-4 rounded-full mr-2"
@@ -38,8 +59,9 @@ const Chart = () => {
           </div>
         </div>
       </div>
-      <div>
+      <div className="w-[50vw] gap-4 ">
         <CardContainer />
+        <AppointmentData />
       </div>
     </div>
   );
